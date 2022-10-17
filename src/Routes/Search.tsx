1c0711eaa140ee15d344bed90fee40c1eb40ef1b
soo <aubepluieh3@naver.com>
2022-10-18 00:03:01 +0900

    #1.14 Search Redirect

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,4 +1,8 @@
+import { useLocation } from "react-router";
 function Search() {
+  const location = useLocation(); // 지금 있는 곳에 관한 정보
+  const keyword = new URLSearchParams(location.search).get("keyword");
+  console.log(keyword);
   return null;
 }
 export default Search;
